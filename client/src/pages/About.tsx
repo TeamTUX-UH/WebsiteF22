5947f9987e99eda010cf757f04a0266cb1d3bb51
John Cox <john.cox0321@gmail.com>
2023-05-14 17:30:29 -0500

    style(about): Add image next to description about team

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -10,7 +10,10 @@ const About: React.FunctionComponent<IInfoProps> = (props) => {
       <div className="Background2">
         <div>
           <p className="SubHeading2">About</p><p className="TeamName">CyberTux</p>
-          <p className="Description">Hi there! We are a group of students who recently completed a semester-long project together called CipherGen. Our goal was to create an engaging decrypting game that would challenge and entertain players while also providing them with an opportunity to improve their coding skills. Over the course of several months, we worked closely together to design, develop, and refine every aspect of the game, from the interface to the algorithms used to generate the ciphers. Despite some bumps in the road along the way, we are incredibly proud of what we have accomplished and are excited to share CipherGen with the world. We hope you enjoy playing it as much as we enjoyed creating it!</p>
+          <div className="container">
+            <img className="imageTux" src="/teamtux.webp" alt="teamtux" />
+            <p className="teamDescription">Hi there! We are a group of students who recently completed a semester-long project together called CipherGen. Our goal was to create an engaging decrypting game that would challenge and entertain players while also providing them with an opportunity to improve their coding skills. Over the course of several months, we worked closely together to design, develop, and refine every aspect of the game, from the interface to the algorithms used to generate the ciphers. Despite some bumps in the road along the way, we are incredibly proud of what we have accomplished and are excited to share CipherGen with the world. We hope you enjoy playing it as much as we enjoyed creating it!</p>
+          </div>
           <p className="Subheading">Meet the Team!</p>
           <div className="GroupPosition">
             <ul className="MemberList">
