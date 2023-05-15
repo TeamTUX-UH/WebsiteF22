05c7419e4f573eaaafa1136ac0ba62a6318eda79
mnguyener <michellehnguyen03@gmail.com>
2023-05-14 20:27:04 -0500

    Create link to the github game

--- client/src/pages/Download.tsx
+++ client/src/pages/Download.tsx
@@ -3,11 +3,21 @@ import "./styles/Info.css";
 
 export interface IDownloadProps {}
 
+// function downloadExe() {
+//   window.location.href="./src/assets/game"
+// };
+
 const Download: React.FunctionComponent<IDownloadProps> = (props) => {
   return (
     <div>
       <div className="Background2">
         <p className="Subheading">Download</p>
+        <p className="Description">Available on: <br/></p>   
+        <a href="https://github.com/TeamTUX-UH/WebsiteF22">
+          <button>
+            INSTALL
+          </button>
+        </a>
       </div>
     </div>
   );
